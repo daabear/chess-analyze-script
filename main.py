5b19b7d459aadee96725198e1bd48b7cab88ef9d
damilkz <marcospizzeria45@gmail.com>
2025-01-09 14:54:37 -0500

    change to main.py, readme

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
-driver = webdriver.Chrome()
+# Input username between quotes
+username = ''
 
+# Options so chrome doesn't automatically close
+chrome_options = Options() 
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+# Get Game Report
 driver.get("https://chess.wintrcat.uk/")
 
+# Click dropdown to open dropdown
 dropdown_btn = driver.find_element(By.ID, 'load-type-dropdown')
 dropdown_btn.click()
 
@@ -19,30 +27,23 @@ dropdown_btn.send_keys(Keys.RETURN)
 # Input username into textarea
 textarea = driver.find_element(By.ID, 'chess-site-username')
 textarea.clear()
-textarea.send_keys('daolqp')
+textarea.send_keys(username) 
 
 # Click the arrow button that appears
 username_verify_btn = driver.find_element(By.CLASS_NAME, 'usernameVerifyBtnIcon')
 username_verify_btn.click()
 
 # Click latest game
-games_listings = WebDriverWait(driver, 2).until(
+games_listings = WebDriverWait(driver, 10).until(
     EC.presence_of_all_elements_located((By.CLASS_NAME, 'game-listing'))
 )
 if games_listings:
     games_listings[0].click()
 
+# Click analyze
 analyze_btn = driver.find_element(By.ID, 'review-button')
 analyze_btn.click()
 
-time.sleep(30)
-
 # Click reCAPTCHA
-options = webdriver.ChromeOptions() 
-options.add_argument("start-maximized")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-
-time.sleep(10)
-
-driver.quit()
+WebDriverWait(driver, 60).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[title='reCAPTCHA']")))
+WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.recaptcha-checkbox-border"))).click()
